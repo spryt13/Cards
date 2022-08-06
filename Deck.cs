2f97c466913d426f26f5b128d8f36fb96cddc942
spryt13 <spryt13@DESKTOP-EC8CJV1>
2022-08-06 10:27:20 +0200

    Add project files.

---
+++ Deck.cs
@@ -0,0 +1,55 @@
+﻿public class Deck
+{
+    public List<Card> cards = new List<Card>();
+    private static Random Random = new Random();
+
+    public void Shuffle()
+    {
+        int b = Random.Next(cards.Count()-1);
+        List<Card> packetOne = new List<Card>();
+        List<Card> packetTwo = new List<Card>();
+        for(int i = 0; i < cards.Count(); i++)
+        {
+            if (i < b)
+                packetOne.Insert(i, cards[i]);
+            else
+                packetTwo.Insert(i-b, cards[i]);
+        }
+        for(int i = 0; i < packetTwo.Count(); i++)
+        {
+            if (i < packetOne.Count())
+                cards[2 * i] = packetTwo[i];
+        }
+        for(int i = 0; i < packetOne.Count(); i++)
+        {
+            if (i < packetTwo.Count())
+                cards[2 * i + 1] = packetOne[i];
+        }
+        if (packetOne.Count() > packetTwo.Count())
+            for(int i = packetTwo.Count(); i < packetOne.Count(); i++)
+            {
+                cards[i + packetTwo.Count()] = packetOne[i];
+            }
+            
+        //shuffle cards here
+    }
+
+    public Card Pick(int number)
+    {
+        Card a = cards[number-1];
+        cards.Remove(a);
+        return a;
+    }
+
+    public Deck(IList<string> symbol, IList<char> sign)
+    {
+        for (int i = 0; i < symbol.Count(); i++)
+        {
+            for (int j = 0; j < sign.Count(); j++)
+            {
+            Card card = new Card(symbol[i], sign[j]);
+            cards.Add(card);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿var symbol = new List<string>(4);
+symbol.Add("hearts");
+symbol.Add("diamonds");
+symbol.Add("clubs");
+symbol.Add("spades");
+
+var sign = new char[] { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '1', 'J', 'Q', 'K'};
+
+Deck bicycle = new Deck(symbol, sign);
+
+{
+    //for (int i = 0; i < 10; i++)
+    {
+        bicycle.Shuffle();
+    }
+
+    int number = Convert.ToInt32(Console.ReadLine());
+    bicycle.Pick(number);
+
+    for (int i = 0; i < bicycle.cards.Count(); i++)
+    {
+        Console.Write(bicycle.cards[i].ToString());
+        Console.WriteLine();
+    }
+}
+
+var karta = new Card(symbol[1], sign[1], 1);
+
+Console.Write(karta.back);
+
+public class Card
+{
+    public string symbol;
+    public char sign;
+    public int back;
+
+    public Card(string a, char b)
+    {
+        symbol = a;
+        sign = b;
+    }
+
+    public Card(string a, char b, int c)
+    {
+        symbol = a;
+        sign = b;
+        back = c * 3;
+    }
+
+    public override string ToString()
+    {
+        return this.sign + " of " + this.symbol;
+    }
+}
