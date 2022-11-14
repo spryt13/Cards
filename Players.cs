61984a083b1442c83ecdc6b2b1d7464a498271c3
Maciej Konieczny <spryt13@gmail.com>
2022-11-14 12:21:01 +0100

    Classes Macau and Players separated from main Program.

---
+++ Players.cs
@@ -0,0 +1,18 @@
+﻿public class Players
+{
+    public List<List<Card>> hands = new List<List<Card>>();
+    public List<string> names = new List<string>();
+
+    public Players()
+    {
+        Console.WriteLine("How many players?");
+        int x = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < x; i++)
+        {
+            var hand = new List<Card>();
+            hands.Add(hand);
+            Console.WriteLine("What's your name, Player " + (i+1) + "?");
+            names.Add(Console.ReadLine());
+        }
+    }
+}
